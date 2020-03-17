5a1542f12b2bf01a4f3bcab020643aeb6c38cc2e
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-03-17 17:09:45 -0400

    after gen in vs(win)

--- WebApi01/WebApplication1/Pages/Index.cshtml.cs
+++ WebApi01/WebApplication1/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,9 +18,19 @@ namespace WebApplication1.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            try
+            {
+                string baseurl = "https://localhost:5001";
+                var client = new HttpClient();
+                swaggerClient foo = new swaggerClient(baseurl, client);
+                var result = await foo.WeatherForecastAsync();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
